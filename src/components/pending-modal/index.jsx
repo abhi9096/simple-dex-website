c8b8a612fbe752819cbcfc8e2687119c9ebe68db
abhi9096 <128750472+abhi9096@users.noreply.github.com>
2026-06-27 23:01:03 +0530

    Update index.jsx

--- src/components/pending-modal/index.jsx
+++ src/components/pending-modal/index.jsx
@@ -1,24 +1,15 @@
 import React from "react";
 import { useWeb3React } from "@web3-react/core";
 import Modal from "react-awesome-modal";
-import { BoxesLoader } from "react-awesome-loaders";
-
 import './pending-modal.css';
 
 const PendingModal = ({ show, width, height, effect, className, onClickAway, message, tx }) => {
-
     const { chainId } = useWeb3React();
-
     return (
         <Modal visible={show} width={width} height={height} className={className} onClickAway={onClickAway} effect={effect}>
             <div className="flex col justify-center align-center pending-container">
                 <div className="flex justify-center loader">
-                    <BoxesLoader
-                        boxColor={"#6366F1"}
-                        style={{ marginBottom: "20px", height: "200px" }}
-                        desktopSize={"100px"}
-                        mobileSize={"80px"}
-                    />
+                    <div className="spinner" style={{ marginBottom: "20px" }}></div>
                 </div>
                 <div className="flex col justify-center align-center state-message">
                     <p className="flex">
